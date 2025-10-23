e6b28697132160d90c77d530bc7b3181d27d7ddb
Nanako7-ix <nanako7.ix@gmail.com>
2025-10-23 20:53:26 +0800

    upd: NTT 修改了一下qwq

--- XCPC/2024KunmingRegional/I.cpp
+++ XCPC/2024KunmingRegional/I.cpp
@@ -38,22 +38,22 @@ private:
 
 using Z = ModInt<u32, 998244353>;
 
-vector<Z> NTT(vector<Z> a, vector<Z> b) {
-	static constexpr Z g = 3;
-	static constexpr u64 P = Z::mod();
-	static vector<Z> rt { 0, 1 };
+template<typename T>
+std::vector<T> NTT(std::vector<T> a, std::vector<T> b) {
+	static constexpr u64 P = T::mod();
+	static constexpr  T  g = 3;
+	static std::vector<T> rt { 0, 1 };
 
 	int tot = a.size() + b.size() - 1;
-	int k = __lg(tot - 1);
-	int n = 1 << (k + 1);
+	int k = std::__lg(tot), n = 1 << (k + 1);
 
-	vector<int> rev(n);
+	std::vector<int> rev(n);
 	for (int i = 0; i < n; ++i) {
 		rev[i] = rev[i >> 1] >> 1 | (i & 1) << k;
 	}
-	
+
 	if ((int) rt.size() < n) {
-		int k = countr_zero(rt.size());
+		int k = std::countr_zero(rt.size());
 		rt.resize(n);
 		for (; (1 << k) < n; ++k) {
 			auto e = g.pow((P - 1) >> (k + 1));
@@ -64,15 +64,14 @@ vector<Z> NTT(vector<Z> a, vector<Z> b) {
 		}
 	}
 
-	auto dft = [&](vector<Z>& f) {
+	auto dft = [&](std::vector<T>& f) {
 		for (int i = 0; i < n; ++i) {
-			if (i < rev[i]) swap(f[i], f[rev[i]]);
+			if (i < rev[i]) std::swap(f[i], f[rev[i]]);
 		}
 		for (int i = 1; i < n; i <<= 1) {
 			for (int j = 0; j < n; j += 2 * i) {
 				for (int k = 0; k < i; ++k) {
-					Z fx = f[j + k];
-					Z fy = f[i + j + k] * rt[i + k];
+					T fx = f[j + k], fy = f[i + j + k] * rt[i + k];
 					f[j + k] = fx + fy;
 					f[i + j + k] = fx - fy;
 				}
@@ -86,15 +85,14 @@ vector<Z> NTT(vector<Z> a, vector<Z> b) {
 	for (int i = 0; i < n; ++i) {
 		a[i] *= b[i];
 	}
-	
-	reverse(a.begin() + 1, a.end());
+
+	std::reverse(a.begin() + 1, a.end());
 	dft(a);
 	a.resize(tot);
-	Z inv = Z(1 - P) / n;
+	T inv = T(n).inv();
 	for (int i = 0; i < tot; ++i) {
 		a[i] *= inv;
 	}
-	
 	return a;
 }
 
